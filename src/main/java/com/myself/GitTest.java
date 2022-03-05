d0890bd50b5c2579996371e6a6ed2d921ff667ce
Arthur <ace2661050889@gmail.com>
2022-03-05 16:45:45 +0800

    fist commit

---
+++ src/main/java/com/myself/GitTest.java
@@ -0,0 +1,14 @@
+package com.myself;
+
+/**
+ * @Author：摘到星星了吗
+ * @Date:2022/3/5 -03-05-16:44
+ * @Description: com.myself
+ * @version:1.0
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello Git!");
+    }
+}
