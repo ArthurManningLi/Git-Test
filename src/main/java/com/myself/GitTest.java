8d3861aae27c9efc16773e956a484c22a62ef4f1
Arthur <ace2661050889@gmail.com>
2022-03-05 16:52:14 +0800

    third commit

--- src/main/java/com/myself/GitTest.java
+++ src/main/java/com/myself/GitTest.java
@@ -13,5 +13,6 @@ public class GitTest {
         System.out.println("hello Git!!");
         System.out.println("hello Git!!!");
         System.out.println("hello Git!!!!");
+        System.out.println("hello Git!!!!!");
     }
 }
